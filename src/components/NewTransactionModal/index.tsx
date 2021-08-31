b1b54b6567fd78921cee8658ae9894b035bf326d
João Bispo <joaobispo2077@gmail.com>
2021-08-31 10:43:21 -0300

    feat: add modal styles

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -13,7 +13,12 @@ export function NewTransactionModal({
   onRequestClose,
 }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpen} onRequestClose={onRequestClose}>
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
       <Container>
         <h2>Cadastrar transação</h2>
 
@@ -22,8 +27,8 @@ export function NewTransactionModal({
 
         {/* <button>Entrada</button>
         <button>Saída</button> */}
-
         <input type="text" placeholder="Categoria" />
+
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
